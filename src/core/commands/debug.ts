cbfd336d87af5845fe8e45f0e05f69e6e3a87e2a
CloudySnow <cloudysn0w.x@gmail.com>
2025-02-28 12:31:14 +0100

    Feat: Updating some stuff

--- src/core/commands/debug.ts
+++ src/core/commands/debug.ts
@@ -4,7 +4,7 @@ import { getDebugInfo } from "@lib/api/debug";
 import { messageUtil } from "@metro/common";
 
 export default () => <ApplicationCommand>{
-    name: "Cum",
+    name: "Cum💦",
     description: Strings.COMMAND_DEBUG_DESC,
     options: [
         {
@@ -16,8 +16,8 @@ export default () => <ApplicationCommand>{
     execute([ephemeral], ctx) {
         const info = getDebugInfo();
         const content = [
-            "**CumCord Debug Info**",
-            `> Revenge: ${info.bunny.version} (${"CumCord " }${info.bunny.loader.version})`,
+            "**💦CumCord Debug Info💦**",
+            `> Revenge: ${info.bunny.version} (${"CumCord💦 " }${info.bunny.loader.version})`,
             `> Discord: ${info.discord.version} (${info.discord.build})`,
             `> React: ${info.react.version} (RN ${info.react.nativeVersion})`,
             `> Hermes: ${info.hermes.version} (bcv${info.hermes.bytecodeVersion})`,
