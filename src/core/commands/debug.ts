d8571354a8e7d3203586a4a4dc8ade02156843e8
Sunnyychi <50956006+sunnyychi@users.noreply.github.com>
2025-03-06 13:41:26 +0100

    Update debug.ts

--- src/core/commands/debug.ts
+++ src/core/commands/debug.ts
@@ -4,7 +4,7 @@ import { getDebugInfo } from "@lib/api/debug";
 import { messageUtil } from "@metro/common";
 
 export default () => <ApplicationCommand>{
-    name:"Cum💦💦",
+    name:"YASS QUEEN🌸🌸",
     description: Strings.COMMAND_DEBUG_DESC,
     options: [
         {
@@ -16,8 +16,8 @@ export default () => <ApplicationCommand>{
     execute([ephemeral], ctx) {
         const info = getDebugInfo();
         const content = [
-            "💦**CumCord Debug Info**💦",
-            `> Revenge: ${info.bunny.version} (${"CumCord💦 " }${info.bunny.loader.version})`,
+            "🌸**EGirlCord Debug Info**🌸",
+            `> Revenge: ${info.bunny.version} (${"EGirlCord🌸 " }${info.bunny.loader.version})`,
             `> Discord: ${info.discord.version} (${info.discord.build})`,
             `> React: ${info.react.version} (RN ${info.react.nativeVersion})`,
             `> Hermes: ${info.hermes.version} (bcv${info.hermes.bytecodeVersion})`,
